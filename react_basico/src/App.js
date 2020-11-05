bb044dd562aaae57f4532bfdd3e5e38544d4439e
Levi <levi-sr@outlook.com>
2020-11-05 20:06:22 -0300

    Component Properties

--- react_basico/src/App.js
+++ react_basico/src/App.js
@@ -7,9 +7,20 @@ function App() {
   return (
     <div className="App">
       <h1>My project</h1>
-      <Comments />
-      <Comments />
-      <Comments />
+      <Comments
+        name="John"
+        email="johndoe@email.com"
+        date={new Date(2020, 3, 9)}
+      >
+        Hello, how are you?
+      </Comments>
+      <Comments
+        name="May"
+        email="marylou@email.com"
+        date={new Date(2020, 3, 11)}
+      >
+        Hi, amazing!
+      </Comments>
     </div>
   );
 }
--- react_basico/src/components/Comments.js
+++ react_basico/src/components/Comments.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 import './Comments.css';
 
-const Comments = () => {
-  return <div className="Comments">Hi, how are you?</div>;
+const Comments = (props) => {
+  return (
+    <div className="Comments">
+      <h2>{props.name}</h2>
+      <p>{props.email}</p>
+
+      {props.children}
+      <p>{props.date.toString()}</p>
+    </div>
+  );
 };
 
 export default Comments;
