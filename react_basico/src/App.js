a04204459085c7c08e17c0ca4c9edb71d5c67609
Levi <levi-sr@outlook.com>
2020-11-05 20:42:50 -0300

    Basic Statefull Component

--- react_basico/src/App.js
+++ react_basico/src/App.js
@@ -1,28 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 import Comments from './components/Comments';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My project</h1>
-      <Comments
-        name="John"
-        email="johndoe@email.com"
-        date={new Date(2020, 3, 9)}
-      >
-        Hello, how are you?
-      </Comments>
-      <Comments
-        name="May"
-        email="marylou@email.com"
-        date={new Date(2020, 3, 11)}
-      >
-        Hi, amazing!
-      </Comments>
-    </div>
-  );
+class App extends Component {
+  state = {
+    comments: [
+      {
+        name: 'Jonh',
+        email: 'johndoe@email.com',
+        date: new Date(2020, 3, 10),
+        message: 'Hello there',
+      },
+      {
+        name: 'Mary Anny',
+        email: 'marylou@email.com',
+        date: new Date(2020, 3, 22),
+        message: 'Amazing',
+      },
+    ],
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>My project</h1>
+
+        {this.state.comments.map((comment, index) => (
+          <Comments
+            key={index}
+            name={comment.name}
+            email={comment.email}
+            date={comment.date}
+          >
+            {comment.message}
+          </Comments>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
