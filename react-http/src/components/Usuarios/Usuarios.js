73405f62d414172dfc77a9b85fe6dbd8122a6237
Levi <levi-sr@outlook.com>
2020-11-11 22:03:21 -0300

    HTTP METHOD|DELETE|FETCH|

--- react-http/src/components/Usuarios/Usuarios.js
+++ react-http/src/components/Usuarios/Usuarios.js
@@ -23,11 +23,15 @@ class Usuarios extends Component {
       window.confirm(
         `Tem certeza que deseja remover "${usuario.nome} ${usuario.sobrenome}"?`
       )
-    ) {
-      let usuarios = this.state.usuarios;
-      usuarios = usuarios.filter((x) => x.id !== usuario.id);
-      this.setState({ usuarios: usuarios });
-    }
+    )
+      fetch(`https://reqres.in/api/users/${usuario.id}`, {
+        method: 'DELETE',
+      }).then((resposta) => {
+        console.log(resposta);
+        let usuarios = this.state.usuarios;
+        usuarios = usuarios.filter((x) => x.id !== usuario.id);
+        this.setState({ usuarios: usuarios });
+      });
   }
 
   componentDidMount() {
