bc9af6fe775d1194bc5ca24ddee40c62055592cd
Levi <levi-sr@outlook.com>
2020-11-11 21:30:39 -0300

    Fetch | GET | Working with JSON

--- react-http/src/components/Usuarios/Usuarios.js
+++ react-http/src/components/Usuarios/Usuarios.js
@@ -30,6 +30,37 @@ class Usuarios extends Component {
     }
   }
 
+  componentDidMount() {
+    // const response = fetch('https://reqres.in/api/users');
+    // console.log(response);
+
+    fetch('https://reqres.in/api/users')
+      .then((resposta) => {
+        // return res.text(); //convertendo a resposta para texto
+        return resposta.json(); //convertendo a resposta para JSON
+      })
+      .then((dados) => {
+        // console.log(dados); //recebe os dados
+        console.log(dados.data);
+
+        //percorrendo o array obtido e transformando as informações para que o estado definido compreenda.
+        const usuarios = dados.data.map((usuario) => {
+          return {
+            id: usuario.id,
+            nome: usuario.first_name,
+            sobrenome: usuario.last_name,
+            email: usuario.email,
+          };
+        });
+
+        console.log(usuarios);
+
+        //atualizando o estado
+        // this.setState({ usuarios: usuarios }); //com isso exibe os usuarios no componente
+        this.setState({ usuarios });
+      });
+  }
+
   render() {
     return (
       <>
