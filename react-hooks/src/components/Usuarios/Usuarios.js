40fcd5aba61431ac273eac07945f2046d141c828
Levi <levi-sr@outlook.com>
2020-11-11 23:12:03 -0300

    React HOOKS | useState | useEffect

---
+++ react-hooks/src/components/Usuarios/Usuarios.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+
+import AdicionarUsuario from '../AdicionarUsuario/AdicionarUsuario';
+import Usuario from '../Usuario/Usuario';
+
+function Usuarios() {
+  const [usuarios, setUsuarios] = useState([]);
+
+  useEffect(() => {
+    fetch('https://reqres.in/api/users')
+      .then((resposta) => {
+        return resposta.json();
+      })
+      .then((dados) => {
+        console.log(dados.data);
+
+        //percorrendo o array obtido e transformando as informações para que o estado definido compreenda.
+        const usuarios = dados.data.map((usuario) => {
+          return {
+            id: usuario.id,
+            nome: usuario.first_name,
+            sobrenome: usuario.last_name,
+            email: usuario.email,
+          };
+        });
+        setUsuarios(usuarios);
+      });
+  }, []);
+
+  const adicionarUsuario = (usuario) => {
+    setUsuarios((usuariosAtuais) => [...usuariosAtuais, usuario]);
+  };
+
+  const removerUsuario = (usuario) => {
+    if (
+      window.confirm(
+        `Tem certeza que deseja remover "${usuario.nome} ${usuario.sobrenome}"?`
+      )
+    )
+      fetch(`https://reqres.in/api/users/${usuario.id}`, {
+        method: 'DELETE',
+      }).then((resposta) => {
+        console.log(resposta);
+        setUsuarios(usuarios.filter((x) => x.id !== usuario.id));
+      });
+  };
+
+  return (
+    <>
+      <AdicionarUsuario adicionarUsuario={adicionarUsuario} />
+
+      {usuarios.map((usuario) => (
+        <Usuario
+          key={usuario.id}
+          usuario={usuario}
+          removerUsuario={() => removerUsuario(usuario)}
+        />
+      ))}
+    </>
+  );
+}
+
+export default Usuarios;
