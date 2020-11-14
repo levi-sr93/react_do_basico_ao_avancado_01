f54c6777d99e6fc37ee66e220aa0ea5c72ad8376
Levi <levi-sr@outlook.com>
2020-11-14 18:26:40 -0300

    Cria componentes básicos de exibição e funções

---
+++ redux-frutaria/src/components/ListaFrutas.jsx
@@ -0,0 +1,22 @@
+import AdicionaFruta from './AdicionaFruta';
+import Fruta from './Fruta';
+
+const ListaFrutas = () => {
+  const frutas = [
+    { id: 1, nome: 'Abacaxi', quantidade: 5 },
+    { id: 2, nome: 'Maçã', quantidade: 2 },
+  ];
+
+  return (
+    <>
+      <h1>Lista de Frutas</h1>
+      <AdicionaFruta />
+
+      {frutas.map((fruta) => (
+        <Fruta key={fruta.id} fruta={fruta} />
+      ))}
+    </>
+  );
+};
+
+export default ListaFrutas;
